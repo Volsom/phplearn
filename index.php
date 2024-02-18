760ed48ed056b1277a18a2a1b44b7eb9fca7a5a6
Volsom <a.moslov@mail.ru>
2024-02-19 00:18:10 +0300

    logo

--- index.php
+++ index.php
@@ -3,15 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Лабораторная 1</title>
+    <title>Практика</title>
 </head>
 <body>
+    <style>
+        .logo {
+            width: 64px;
+            height: 64px;
+        }
+        footer {
+            position: fixed;
+            bottom: 0;
+        }
+    </style>
     <header>
-        <img src="/img/5868.upto100x100.png" alt="Logo Polytech">
+        <img class="logo" src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech">
     </header>
     <main>
         <?php
             echo '<h1>Hello, world!</h1>';
+            $url = 'http://www.example.com';
+            print_r(get_headers($url));
         ?>
     </main>
     <footer>
--- lab1/index.php
+++ lab1/index.php
@@ -7,9 +7,19 @@
 </head>
 <body>
     <header>
-        <img src="/img/5868.upto100x100.png" alt="Logo Polytech">
+        <img src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech">
     </header>
     <main>
+    <style>
+        .logo {
+            width: 64px;
+            height: 64px;
+        }
+        footer {
+            position: fixed;
+            bottom: 0;
+        }
+    </style>
         <?php
             echo '<h1>Hello, world!</h1>';
         ?>
