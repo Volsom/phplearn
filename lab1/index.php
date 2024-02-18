375eebc5d5dbc39afbd0bf85b080458f30b3d2f2
Volsom <a.moslov@mail.ru>
2024-02-18 22:14:31 +0300

    lab1

---
+++ lab1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лабораторная 1</title>
+</head>
+<body>
+    <header>
+        <img src="/img/5868.upto100x100.png" alt="Logo Polytech">
+    </header>
+    <main>
+        <?php
+            echo '<h1>Hello, world!</h1>';
+        ?>
+    </main>
+    <footer>
+        <p>Мослов Александр 231-323</p>
+    </footer>
+</body>
+</html>
