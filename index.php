5015ae2b65ae9348e7c0266eacf82c864fa93f53
Volsom <a.moslov@mail.ru>
2024-02-19 20:49:17 +0300

    V1

--- index.php
+++ index.php
@@ -7,24 +7,103 @@
 </head>
 <body>
     <style>
+        html {
+            width: 100%;
+            height: 100%;
+            font-family: monospace;
+        }
         .logo {
+            display: block;
+            object-fit: contain;
+            object-position: left;
+            width: 100%;
+            height: 100%;
+        }
+        header {
+            display: flex;
+            align-items: center;
+            background-color: darkgray;
+            height: 84px;
+            justify-content: space-between;
+            padding-left: 10px;
+            padding-right: 40px;
+        }
+        .logo__wrapper {
             width: 64px;
             height: 64px;
+            background-color: unset;
         }
         footer {
-            position: fixed;
-            bottom: 0;
+            display: flex;
+            align-items: center;
+            background-color:darkgray;
+            color: black;
+            font-weight: 700;
+            height: 80px;
+        }
+        body {
+            background-color: lightslategrey;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            min-height: 100%;
+            display: grid;
+            grid-template-rows: auto 1fr auto;
+        }
+        h1 {
+            margin: 0;
+            padding: 10px;
+        }
+        p {
+            padding-left: 10px;
+            margin: 0;
+        }
+        div {
+            height: 100px;
+            background-color: brown;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+            position: relative;
+            font-size: 16px;
+            font-weight: 700;
+        }
+        a::after {
+            position: absolute;
+            content: '';
+            left: 0;
+            bottom: -10px;
+            display: block;
+            width: 100%;
+            height: 3px;
+            background: black;
+            color: black;
+            background-color: black;
+        }
+        a::after {
+            width: 0;
+            transition: 0.3s;
+        }
+        a:hover:after {
+            width: 100%;
         }
     </style>
     <header>
-        <img class="logo" src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech">
+        <div class="logo__wrapper"><img class="logo" src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech"></div>
+        <a href="/phplearn/lab1">Lab1</a>
     </header>
     <main>
         <?php
             echo '<h1>Hello, world!</h1>';
-            $url = 'http://www.example.com';
-            print_r(get_headers($url));
+            $expression = 0;
         ?>
+        <?= '<p>напечатать эту строку</p>' ?>
+        <?php if ($expression == true): ?>
+            <p>Это будет отображено, если выражение истинно.</p>
+        <?php else: ?>
+            <p>В ином случае будет отображено это.</p>
+        <?php endif; ?>
     </main>
     <footer>
         <p>Мослов Александр 231-323</p>
--- lab1/index.php
+++ lab1/index.php
@@ -6,20 +6,94 @@
     <title>Лабораторная 1</title>
 </head>
 <body>
-    <header>
-        <img src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech">
-    </header>
-    <main>
     <style>
+        html {
+            width: 100%;
+            height: 100%;
+            font-family: monospace;
+        }
         .logo {
+            display: block;
+            object-fit: contain;
+            object-position: left;
+            width: 100%;
+            height: 100%;
+        }
+        header {
+            display: flex;
+            align-items: center;
+            background-color: darkgray;
+            height: 84px;
+            justify-content: space-between;
+            padding-left: 10px;
+            padding-right: 40px;
+        }
+        .logo__wrapper {
             width: 64px;
             height: 64px;
+            background-color: unset;
         }
         footer {
-            position: fixed;
-            bottom: 0;
+            display: flex;
+            align-items: center;
+            background-color:darkgray;
+            color: black;
+            font-weight: 700;
+            height: 80px;
+        }
+        body {
+            background-color: lightslategrey;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            min-height: 100%;
+            display: grid;
+            grid-template-rows: auto 1fr auto;
+        }
+        h1 {
+            margin: 0;
+            padding: 10px;
+        }
+        p {
+            padding-left: 10px;
+            margin: 0;
+        }
+        div {
+            height: 100px;
+            background-color: brown;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+            position: relative;
+            font-size: 16px;
+            font-weight: 700;
+        }
+        a::after {
+            position: absolute;
+            content: '';
+            left: 0;
+            bottom: -10px;
+            display: block;
+            width: 100%;
+            height: 3px;
+            background: black;
+            color: black;
+            background-color: black;
+        }
+        a::after {
+            width: 0;
+            transition: 0.3s;
+        }
+        a:hover:after {
+            width: 100%;
         }
     </style>
+    <header>
+        <div class="logo__wrapper"><img class="logo" src="/phplearn/img/5868.upto100x100.png" alt="Logo Polytech"></div>
+        <a href="/phplearn">HOME</a>
+    </header>
+    <main>
         <?php
             echo '<h1>Hello, world!</h1>';
         ?>
