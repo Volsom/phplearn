c78e9e07d4cc6a6fe366d4a8fe0b26efb2249e1f
Volsom <a.moslov@mail.ru>
2024-03-14 16:24:25 +0300

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,96 @@
+let string = '';
+        function alerted1(){
+          string += '1';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted2(){
+          string += '2';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted3(){
+          string += '3';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted4(){
+          string += '/';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted5(){
+          string += '4';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted6(){
+          string += '5';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted7(){
+          string += '6';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted8(){
+          string += '*';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted9(){
+          string += '7';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted10(){
+          string += '8';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted11(){
+          string += '9';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted12(){
+          string += '-';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted13(){
+          string += '0';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted14(){
+          string = '';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted15(){
+          string += '+';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted16(){
+          string += '.';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted17(){
+          string += '(';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted18(){
+          string += ')';
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
+        function alerted19(){
+          string = string.slice(0, -1);
+          const inputElement = document.querySelector('#rezult');
+          inputElement.value = string;
+        }
